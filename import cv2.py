ecfe11d86c2a819dec84ee5fb054a8dadd3d2c28
Cleber santos <112781018+cleber1santos@users.noreply.github.com>
2023-12-14 19:07:39 -0300

    Update import cv2.py

--- import cv2.py
+++ import cv2.py
@@ -2,20 +2,25 @@ import cv2
 
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
-cap= cv2.VideoCapture(0)
+cap = cv2.VideoCapture(0)
+cap.set(3, 640) 
+cap.set(4, 480)  
 
 while True:
-    red , img = cap.read()
+    ret, img = cap.read()
     
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     
-    faces = face_cascade.detectMultiScale( gray, 1.1 ,5)
-    for (x,y,w,h) in faces:
-        cv2.rectangle(img, (x,y) , (x+w,y+h) , (255,0,0), 2)
-        roy_gray = gray [y:y+h , x:x+w]
-        ray_color = img[y:y+h , x:x+w]
+    faces = face_cascade.detectMultiScale(gray, 1.1, 5)
+    for (x, y, w, h) in faces:
+        cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
+        roi_gray = gray[y:y+h, x:x+w]
+        roi_color = img[y:y+h, x:x+w]
         
-    cv2.imwrite('face detectada.jpg',img)
+    cv2.imshow('Detecção de Rosto', img)
     
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
 cap.release()
 cv2.destroyAllWindows()
